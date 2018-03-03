d0aa45a3eeee109050d88c58c77aa2f96149aa93
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2018-03-02 23:49:58 -0300

    Created modal windows

--- src/demo/App.js
+++ src/demo/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 import {
   Container,
@@ -10,45 +10,71 @@ import {
   Separator,
   TabContainer,
   TabItem,
-  Button
+  Button,
+  Modal
 } from "../lib";
 
-const App = () => (
-  <Container vertical>
-    <Navigation align="stretch">
-      <NavigationItem grow>Starry Night Components</NavigationItem>
-      <NavigationItem href="./manual">Docs</NavigationItem>
-      <NavigationItem href="https://github.com/holywyvern/starry-night-ui">
-        Github
-      </NavigationItem>
-    </Navigation>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalOpen: false
+    };
+  }
 
-    <Separator />
-    <Panel grow>
-      <Navigation vertical>
-        <NavigationGroup title="Components">
-          <NavigationItem>Link 1</NavigationItem>
-        </NavigationGroup>
-      </Navigation>
-      <Separator />
-      <Panel vertical grow>
-        <TabContainer onClose={console.log}>
-          <TabItem title="Tab 1">Tab 1</TabItem>
-          <TabItem title="Tab 2">Tab 2</TabItem>
-          <TabItem title="Tab 3">Tab 3</TabItem>
-          <TabItem title="Tab 4">Tab 4</TabItem>
-          <TabItem title="Tab 5" disabled>
-            Tab 5
-          </TabItem>
-        </TabContainer>
-        <HorizontalLayout justify="end" padding="0 10px 10px 0">
-          <Button>Accept</Button>
-          <Button>Cancel</Button>
-          <Button disabled>Disabled</Button>
-        </HorizontalLayout>
-      </Panel>
-    </Panel>
-  </Container>
-);
+  toggleModal = () => {
+    const { modalOpen } = this.state;
+    this.setState({ modalOpen: !modalOpen });
+  };
+
+  render() {
+    const { modalOpen } = this.state;
+    return (
+      <Container vertical>
+        <Navigation align="stretch">
+          <NavigationItem grow>Starry Night Components</NavigationItem>
+          <NavigationItem href="./manual">Docs</NavigationItem>
+          <NavigationItem href="https://github.com/holywyvern/starry-night-ui">
+            Github
+          </NavigationItem>
+        </Navigation>
+
+        <Separator />
+        <Panel grow>
+          <Navigation vertical>
+            <NavigationGroup title="Components">
+              <NavigationItem>Link 1</NavigationItem>
+            </NavigationGroup>
+          </Navigation>
+          <Separator />
+          <Panel vertical grow>
+            <TabContainer onClose={console.log}>
+              <TabItem title="Tab 1">Tab 1</TabItem>
+              <TabItem title="Tab 2">Tab 2</TabItem>
+              <TabItem title="Tab 3">Tab 3</TabItem>
+              <TabItem title="Tab 4">Tab 4</TabItem>
+              <TabItem title="Tab 5" disabled>
+                Tab 5
+              </TabItem>
+            </TabContainer>
+            <HorizontalLayout justify="end" padding="0 10px 10px 0">
+              <Button>Accept</Button>
+              <Button>Cancel</Button>
+              <Button disabled>Disabled</Button>
+              <Button onClick={this.toggleModal}>Open Modal</Button>
+            </HorizontalLayout>
+          </Panel>
+        </Panel>
+        <Modal
+          open={modalOpen}
+          onClose={this.toggleModal}
+          title="Hi, I'm a Modal"
+        >
+          Close me PLZ!
+        </Modal>
+      </Container>
+    );
+  }
+}
 
 export default App;
--- src/lib/components/Modal.js
+++ src/lib/components/Modal.js
@@ -1,13 +1,73 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
+import classNames from "classnames";
+
+import "./Modal.scss";
+
 /**
  * A modal is an element than shows over the screen, stopping all user interaction
  * from the rest of the site until the Modal is closed
  */
 class Modal extends Component {
+  static propTypes = {
+    /** The title of the modal window */
+    title: PropTypes.node,
+    /** If true, the modal will be open */
+    open: PropTypes.bool,
+    /**
+     * Callback for when closing the window, if no one is provided,
+     * no close button will be present.
+     */
+    onClose: PropTypes.func,
+    /**
+     * Indicates than clicking on the background will do the same as the close action.
+     */
+    closeOnBackgroundClick: PropTypes.func,
+    /** Indicates the body of the modal is oriented vertically instead of horizontally */
+    vertical: PropTypes.bool
+  };
+
+  static defaultProps = {
+    open: false,
+    vertical: false,
+    closeOnBackgroundClick: false
+  };
+
+  stopPropagation(event) {
+    event.stopPropagation();
+  }
   render() {
-    return null;
+    const {
+      children,
+      title,
+      open,
+      onClose,
+      vertical,
+      closeOnBackgroundClick
+    } = this.props;
+    const classes = classNames("sn-modal-container", { open });
+    const bodyClasses = classNames("body", { vertical });
+    return (
+      <div
+        className={classes}
+        onClick={closeOnBackgroundClick ? onClose : undefined}
+      >
+        <div className="sn-modal" onClick={this.stopPropagation}>
+          {title || onClose ? (
+            <div className="header">
+              <h3>{title}</h3>
+              {onClose ? (
+                <button type="button" onClick={onClose}>
+                  &times;
+                </button>
+              ) : null}
+            </div>
+          ) : null}
+          <div className={bodyClasses}>{children}</div>
+        </div>
+      </div>
+    );
   }
 }
 
