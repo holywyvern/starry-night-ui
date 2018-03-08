0ecbe11139cc7b696519a1d1c7e3fe442a6f815e
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2018-03-08 13:46:38 -0300

    Starting the list component. And fixed an scrollbar issue in firefox

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
 # starry-night-ui
 
 A React user interface for applications (Mostly Electron apps)
+
+## Installing
+
+```sh
+npm i --save starry-night-ui
+```
+
+## Usage
+
+Import the required CSS in your code:
+
+```js
+import "starry-night-ui/build/css/index.css";
+```
+
+Wrap your application in a Container:
+
+```jsx
+import { Container } from "starry-night-ui";
+
+const App = () => {
+    return (
+        <Container>
+            {...}
+        </Container>
+    );
+};
+```
+
+And you are good to go now!
+
+## Documentation
+
+[Read the style guide](https://holywyvern.github.io/starry-night-ui/manual/) to learn about each component on the library.
+
+And [check out the demo](https://holywyvern.github.io/starry-night-ui) to see this library in action.
+
+## License
+
+[Apache 2.0](https://spdx.org/licenses/Apache-2.0.html)
--- src/lib/components/List.js
+++ src/lib/components/List.js
@@ -1,7 +1,10 @@
-import React, { Component } from "react";
+import React, { Component, Children } from "react";
 import PropTypes from "prop-types";
 
 import layoutElement from "./layoutElement";
+import PerfectScrollbar from "react-perfect-scrollbar";
+
+import "./List.scss";
 
 /**
  * A list is a vertical set of elements.
@@ -9,11 +12,63 @@ import layoutElement from "./layoutElement";
  * Users can click on a list to select it.
  */
 class List extends Component {
-  static propTypes = {};
+  static propTypes = {
+    selected: PropTypes.any,
+    onSelectionChange: PropTypes.func,
+    valueComparator: PropTypes.func,
+    multiple: PropTypes.bool
+  };
   static defaultProps = {};
+
+  constructor(props) {
+    super(props);
+    this.state = { selected: props.selected };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (typeof nextProps.selected !== "undefined") {
+      this.setState({ selected: nextProps.selected });
+    }
+  }
+
+  onItemSelect(value) {}
+
+  _isChildSelected(value) {
+    const { selected } = this.state;
+    const { multiple, valueComparator } = this.props;
+    if (multiple && Array.isArray(selected)) {
+      if (typeof valueComparator === "function") {
+        return selected.filter(i => valueComparator(i, value)).length > 0;
+      }
+      return selected.indexOf(value) !== -1;
+    }
+    if (typeof valueComparator === "function") {
+      return valueComparator(selected, value);
+    }
+    return selected === value;
+  }
+
   render() {
     const { children, style } = this.props;
-    return <ul style={style}>{children}</ul>;
+    return (
+      <div style={style} className="sn-list">
+        <PerfectScrollbar>
+          <ul>
+            {Children.map(children, (child, index) => {
+              if (React.isValidElement(child)) {
+                const value = child.props.value || index;
+                return React.cloneElement(child, {
+                  value,
+                  selected: this._isChildSelected(value),
+                  onSelect: this.onItemSelect.bind(value)
+                });
+              }
+              return null;
+            })}
+          </ul>
+        </PerfectScrollbar>
+      </div>
+    );
   }
 }
 
